3569528216dd5a4f0c920d88c4c4ff966de2c484
robin <robin@Robin-Workstation>
2017-04-06 18:51:16 +0200

    evenement object

--- API/Calendar/quickstart.php
+++ API/Calendar/quickstart.php
@@ -14,6 +14,7 @@ define('SCOPES', implode(' ', array(
   Google_Service_Calendar::CALENDAR_READONLY)
 ));
 
+$evenements = array();
 
 /**
  * Returns an authorized API client.
@@ -94,6 +95,34 @@ if (count($results->getItems()) == 0) {
     if (empty($start)) {
       $start = $event->start->date;
     }
+    $evenement = new Evenement($locationEvent, $start, $event->getSummary());
+    array_push($evenements, $evenement);
     printf("%s (%s)\n%s", $event->getSummary(), $start, $locationEvent);
   }
 }
+
+class Evenement{
+	public $location;
+	public $date;
+	public $summary;
+	
+	public function __construct($location, $date, $summary){
+		$this->location = $location;
+		$this->date = $date;
+		$this->summary = $summary;
+	}
+	
+	public function getLocation(){
+		return $this->location;
+	}
+	
+	public function getDate(){
+		return $this->date;
+	}
+	
+	public function getSummary(){
+		return $this->summary;
+	}
+}
+
+var_dump($evenements);
